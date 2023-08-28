ccce6aab58b55194215073a4b3c88524b8d16df2
EmilienKopp <emilien.kopp@gmail.com>
2023-08-28 16:29:01 +0900

    august 29 demo

---
+++ src/lib/UserInput.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let placeholder: string = "Enter your message here";
+
+    let message: string = "Hello world!";
+    let apiResponse: string = "";
+
+    async function postMessage() {
+        const response = await fetch('http://localhost:5173/API/mock', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message })
+        });
+
+        const data = await response.json();
+
+        apiResponse = data.message;
+    }
+
+</script>
+
+<div>
+
+    <input type="text" bind:value={message} placeholder={placeholder} />
+    <button on:click={postMessage}>Send</button>
+</div>
+
+<div>
+    
+    <p>Message: {message}</p>
+    <p>Response: {apiResponse}</p>
+
+</div>
