c4827d4d6bcdc91c38fed71b527122a5bb2ea1d1
EmilienKopp <emilien.kopp@gmail.com>
2023-09-01 12:13:51 +0900

    feat: lesson 2 demo content

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,6 @@
 
 <main>
 	<h1>Vite + Svelte</h1>
-	<Counter />
 	<UserInput />
 </main>
 
--- src/lib/UserInput.svelte
+++ src/lib/UserInput.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-    import { fly } from "svelte/transition";
+    import { fly, slide } from "svelte/transition";
     export let placeholder: string = "Enter your message here";
 
     let message: string = "Hello world!";
     let apiResponse: string = "";
+
+    // ⇓ Hands on ⇓
+    let dialogsArray: {content: string, response: string} [] = [] //TS
+    let dialogsArrayJS = []; //JS
+    // ⇑ Hands on ⇑
 
     async function postMessage() {
         const response = await fetch('https://one-in-emilien.com/API/mock', {
@@ -15,18 +20,58 @@
         });
 
         const data = await response.json();
-        console.table(data);
-        apiResponse = data.message;
+        
+        const dialog = { content: message, response: data.message };
+        console.table(dialog);
+        // ⇓ Hands on ⇓
+        dialogsArray = [...dialogsArray, dialog] //TS
+        dialogsArrayJS = [...dialogsArrayJS, dialog] //JS
+        // ⇑ Hands on ⇑
+
+        return dialog;
     }
+
+    async function handleClick() {
+        promise = postMessage();
+    }
+    
+    let promise: Promise<any>;
+    // JS: let promise;
 
 </script>
 
-<div>
+<fieldset>
+    <legend>Send a message</legend>
     <input type="text" bind:value={message} placeholder={placeholder} />
-    <button on:click={postMessage}>Send</button>
-</div>
+    <button on:click={handleClick}>Send</button>
+    {#await promise}
+        <p>Waiting for response...</p>
+    {:then res}
+        <p>Latest response: {res?.response ?? ''}</p>
+    {:catch error}
+        <p style="color: red">{error?.message}</p>
+    {/await}
+</fieldset>
 
 <div>
-    <p>Message: {message}</p>
-    <p>Response: {apiResponse}</p>
+    {#if dialogsArray.length}
+        <h2>Messages</h2>
+    {:else}
+        <p>No messages yet</p>
+    {/if}
+
+    {#each dialogsArray as dialog} 
+        <p in:slide>Sent: {dialog.content}</p>
+        <p in:slide>Received: {dialog.response}</p>
+    {/each}
+    
+    <!-- {#each dialogsArray as {content, response} }
+        <p>Sent: {content}</p>
+        <p>Received: {response}</p>
+    {/each} -->
+   
 </div>
+
+<style>
+
+</style>
