200426ca0cfcf6815ed8dd1dd71a6fb78582cc13
EmilienKopp <emilien.kopp@gmail.com>
2023-10-17 11:31:30 +0900

    handson prep

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
-export const count = writable(0);
+// Create a 'count' store that is a writable store, defaulting at 0
 
 export const status = writable('offline');
